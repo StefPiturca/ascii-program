f5e10e19d95ec01ddbb77ef90c260adf491bb612
StefPiturca <74204702+StefPiturca@users.noreply.github.com>
2020-12-29 13:08:00 +0000

    tema

---
+++ numbers.js
@@ -0,0 +1,21 @@
+var a=string.character(65);
+String.fromCharCode(65,66,67);
+var lineBreak = ascii("\n");
+function myFunction(){
+    var str=document.getElementById("id1");
+    if (str.value=="") {
+       str.focus();
+       return;
+    }
+    var a="ASCII Code is == >  ";
+document.getElementById("demo").innerHTML =a+str.value.charCodeAt(0);
+}
+while (A=string.character(65)); and (B=string.character(66));
+C=string.character(67);
+D=string.character(68); 
+var str = "HELLO WORLD";
+var n = str.charCodeAt(str.length-1);
+addEventListener=button.appendChild(1);
+document.write("The AsCii numbers are: 65=A  66=B 67=c until W=120");
+document.write("The AsCII numbers are:69 E  70 F 71 G until X 110");
+ 
